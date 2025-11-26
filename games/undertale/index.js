c88ed28922656135b199acb26819dedec80ca4cd
securecentrix81 <135786319+securecentrix81@users.noreply.github.com>
2025-11-26 09:02:07 -0800

    Update index.js

--- games/undertale/index.js
+++ games/undertale/index.js
@@ -125,11 +125,12 @@ var Module = {
 Module.setStatus("Downloading...");
 window.onerror = function (event) {
   // TODO: do not warn on ok events like simulating an infinite loop or exitStatus
-  Module.setStatus("Exception thrown, see JavaScript console");
+  /*Module.setStatus("Exception thrown, see JavaScript console");
   spinnerElement.style.display = "none";
   Module.setStatus = function (text) {
     if (text) Module.printErr("[post-exception status] " + text);
-  };
+  };*/
+  console.error(event)
 };
 
 // Route URL GET parameters to argc+argv
